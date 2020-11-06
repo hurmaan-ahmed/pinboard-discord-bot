a7c0efda224ceb43008e27a14818ee2216038f43
Hurmaan Ahmed <hurmaanahmed@Hurmaans-MBP.fios-router.home>
2020-11-06 00:24:11 -0500

    Updated discord bot code to include different commands

--- bot.py
+++ bot.py
@@ -1,24 +1,51 @@
 import discord
 from discord.ext import commands
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from datetime import date
 
+# Initialize Firebase credentials
+cred = credentials.Certificate('./discord-pin-bot-cred.json')
+firebase_admin.initialize_app(cred)
+
+# Start up the firebase client
+db = firestore.client()
+
+# Set command prefix for the bot
 bot = commands.Bot(command_prefix = '!')
 
+# Runs when the bot comes online
 @bot.event
 async def on_ready():
     print('Bot online')
 
-@bot.event #command recognition
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    if message.content.startswith('!commands'):
-        await message.channel.send('Available commands: !pinboards, !newpb')
-    if message.content.startswith('!pinboards'):
-        await message.channel.send('To be implemented, show a list of pinboards')
-    if message.content.startswith('!cdaily'):
-        await message.channel.send('To be implemented; create a new pinboard')
+# Creates the board
+@bot.command()
+async def create(ctx, name):
+    board_name_ref = db.collection('boards').document(f'{name}')
+    if board_name_ref.get().exists:
+        await ctx.send(f"The following board: '{name}' already exists! :x:")
+    else:
+        # messages_ref = board_name_ref.collection("messages").document(ctx.message.author.id).set
+        today = date.today()
+        board_name_ref.set(
+            {
+                "createdBy": ctx.message.author.id,
+                "dateCreated": today.strftime("%d/%m/%Y")
+            }
+        )
+        await ctx.send(f"The board: '{name}' was created! :white_check_mark:")
 
+# Add permissions to only allow user who created their board, or admin to delete specific board
+@bot.command()
+async def delete(ctx, name):
+    board_name_ref = db.collection('boards').document(f'{name}')
+    if board_name_ref.get().exists == False:
+        await ctx.send(f"The following board: '{name}' does not exist! :slight_frown:")
+    else:
+        board_name_ref.delete()
+        await ctx.send(f"The following board: '{name}' was deleted! :white_check_mark:")
 
-
-
-bot.run('NzY3NDk5MzM1MjM5NDY3MDA4.X4yzdA.j6Ryhhwww9M0zxEgra3yIHTAcC8')#bot token
+# Bot token
+bot.run('TOKEN GOES HERE')
